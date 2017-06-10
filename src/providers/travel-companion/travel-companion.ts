465a801fd20303a0069fd3465aeba19b786dcbcb
Adisesh, Maaya <maaya.adishesh@ge.com>
2017-06-10 20:56:16 +0530

    passing test cases for travel companion

--- src/providers/travel-companion/travel-companion.ts
+++ src/providers/travel-companion/travel-companion.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Http } from '@angular/http';
 import 'rxjs/add/operator/map';
+import { CompanionCriteria } from '../../models/companion-criteria.model'
 
 /*
   Generated class for the TravelCompanionProvider provider.
@@ -10,9 +11,25 @@ import 'rxjs/add/operator/map';
 */
 @Injectable()
 export class TravelCompanionProvider {
-
+ private res = "";
   constructor(public http: Http) {
     console.log('Hello TravelCompanionProvider Provider');
   }
-
+  searchNearByRestaurants(criteria: CompanionCriteria) : string{
+    console.log("Inside add method");
+    console.log("location: " +  criteria.location);
+    console.log("radius: " +  criteria.radius);
+    console.log("type: " +  criteria.type);
+    console.log("keyword: " +  criteria.keyword);
+    var endpoint = 'https://maps.googleapis.com/maps/api/place/nearbysearch/json?'
+                + 'location=' + criteria.location + 
+                '&radius=' + criteria.radius +
+                 '&type=' + criteria.type +
+                 '&keyword=' + criteria.keyword +
+                 '&key=AIzaSyC4x3E86QCk1dW2iVA5GHmRH9mNKtZx-1g';
+     this.http.get(endpoint).map(response => response.json())
+    .subscribe((json : Object) => this.res = json.toString());
+    console.log(this.res);
+    return this.res;
+  }
 }
