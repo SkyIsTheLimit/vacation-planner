5ab4d0796650570c63497fe1a03f6ce25818ad20
Harsh Kumar <harshumar1@gmail.com>
2017-06-18 23:13:58 +0530

    Notification Implementation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,7 @@ import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
 import { TabsPage } from '../pages/tabs/tabs';
-import { DiscoverPage, DispatchPage } from '../pages';
+import { DiscoverPage, DispatchPage, NotificationPage } from '../pages';
 
 @Component({
   templateUrl: 'app.html'
