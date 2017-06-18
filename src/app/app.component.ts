eee5566bf89c8d995992b0bb05f04d8e9aa850cf
Sandeep Prasad <s3g007@gmail.com>
2017-06-18 12:52:09 +0530

    Added a few controls to discover page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,7 @@ import { Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
-import { DispatchPage } from '../pages';
+import { DiscoverPage, DispatchPage } from '../pages';
 @Component({
   templateUrl: 'app.html'
 })
