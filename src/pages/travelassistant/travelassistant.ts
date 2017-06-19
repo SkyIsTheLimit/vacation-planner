a26d96f37c2e68e22aa7f15066db55ce7528b80b
Sandeep Prasad <s3g007@gmail.com>
2017-06-20 00:42:50 +0530

    Created placeholder for saving new users to database.

--- src/pages/travelassistant/travelassistant.ts
+++ src/pages/travelassistant/travelassistant.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
+import { TravelCompanionProvider } from '../../providers';
+
+import { Restaurant } from '../../models/restaurant';
 /**
  * Generated class for the TravelassistantPage page.
  *
@@ -14,11 +17,15 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class TravelassistantPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, private tc: TravelCompanionProvider) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad TravelassistantPage');
+    this.loadNearbyPlaces();
+  }
+
+  loadNearbyPlaces() {
   }
 
 }
